2874018bbd583f757446c46a7118665ff726c966
SONO <son.stream8580@gmail.com>
2020-06-06 22:48:43 +0900

    Add awaiter

---
+++ Assets/Scripts/WodiLib/UnityUtil/WebRequestAwaiter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace WodiLib.UnityUtil
+{
+    public class WebRequestAwaiter : INotifyCompletion
+    {
+        private UnityWebRequestAsyncOperation asyncOp;
+        private Action continuation;
+
+        public WebRequestAwaiter(UnityWebRequestAsyncOperation asyncOp)
+        {
+            this.asyncOp = asyncOp;
+            asyncOp.completed += OnRequestCompleted;
+        }
+
+        public bool IsCompleted { get { return asyncOp.isDone; } }
+
+        public void GetResult() { }
+
+        public void OnCompleted(Action continuation)
+        {
+            this.continuation = continuation;
+        }
+
+        private void OnRequestCompleted(AsyncOperation obj)
+        {
+            continuation?.Invoke();
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static WebRequestAwaiter GetAwaiter(this UnityWebRequestAsyncOperation asyncOp)
+        {
+            return new WebRequestAwaiter(asyncOp);
+        }
+    }
+}
